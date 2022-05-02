c57d09c3badd10c065cea20272244bc21b68b2bc
Elaine <elainefelipemoreira@gmail.com>
2022-05-02 12:55:56 -0300

    feat: 🎸 add route Login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Redirect,Route, Switch } from 'react-router-dom'
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-
+import Login from './pages/Login';
+// import rockGlass from './images/rockGlass.svg';
+// consulta ao - https://v5.reactrouter.com/web/api/Redirect
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Redirect exact from="/" to="/login"/>
+      <Route path="/login">
+        <Login/>
+      </Route>
+    </Switch>
+    // <div className="App">
+    //   <span className="logo">TRYBE</span>
+    //   <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+    //     Glass
+    //   </object>
+    // </div>
   );
 }
 
