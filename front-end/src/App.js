ca0b3af8e9762d3212f8c8bae37c15cd794ca68b
Erick Santos <erickosantos.dev@gmail.com>
2022-05-02 19:38:34 -0300

    feat: 🎸 Adding register route and page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 // consulta ao - https://v5.reactrouter.com/web/api/Redirect
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={ <Navigate replace to="/login" /> } />
           <Route path="/login" element={ <Login /> } />
+          <Route path="/register" element={ <Register /> } />
         </Routes>
       </BrowserRouter>
     </div>
