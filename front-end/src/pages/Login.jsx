a0a28c5b41cd6556b594cdac7aa0974be8ef06a6
Elaine <elainefelipemoreira@gmail.com>
2022-05-02 13:05:51 -0300

    feat: 🎸 create page Login

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormLogin from '../components/FormLogin';
+
+function Login() {
+  return(
+    <main>
+      <h1>App Delivery</h1>
+      <FormLogin />
+    </main>
+  )
+}
+
+export default Login
