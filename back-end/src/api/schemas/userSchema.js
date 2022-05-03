9d231292ab6d269ff2620f3e08b17f138c4f8680
icarofgomes <icaro.iky@gmail.com>
2022-05-03 15:47:47 -0300

    Add schemas

---
+++ back-end/src/api/schemas/userSchema.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+module.exports = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required().messages({
+    'string.min': '"password" length must be 6 characters long',
+  }),
+  image: Joi.string(),
+});
