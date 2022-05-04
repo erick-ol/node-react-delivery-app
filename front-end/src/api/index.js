dc05bda949a0451e67884e07c741432cff74091b
Erick Santos <erickosantos.dev@gmail.com>
2022-05-04 17:16:30 -0300

    feat: 🎸 Adding REGISTER_POST api

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -10,3 +10,14 @@ export const LOGIN_POST = (body) => ({
     body: JSON.stringify(body),
   },
 });
+
+export const REGISTER_POST = (body) => ({
+  url: `${API_URL}/register`,
+  options: {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  },
+});
