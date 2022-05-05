d9137dda1c0b6fac0b9bc8e9fc77bde850ac8caf
Mariana Garcia <maridgarcia045@gmail.com>
2022-05-05 20:22:42 -0300

    style: 💄 fix lint

--- back-end/src/api/services/saleProductService.js
+++ back-end/src/api/services/saleProductService.js
@@ -1,10 +1,10 @@
-const {SaleProduct} = require('../../database/models/saleproduct');
+const { saleProduct } = require('../../database/models/saleproduct');
 
 const SALE_PRODUCT_ID = 'product_id';
 
 const createSaleProduct = async (id, quantity) => {
-  console.log('antes', SaleProduct );
-  const product = await SaleProduct.create({ [SALE_PRODUCT_ID]: id, quantity });
+  console.log('antes', saleProduct);
+  const product = await saleProduct.create({ [SALE_PRODUCT_ID]: id, quantity });
   console.log(product);
   return product.product_id;
 };
