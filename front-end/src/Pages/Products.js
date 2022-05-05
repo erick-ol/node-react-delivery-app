ed816e47f586762799a9800ae00b940747d9944e
Elaine <elainefelipemoreira@gmail.com>
2022-05-05 14:51:37 -0300

    feat: 🎸 add page and components products

---
+++ front-end/src/Pages/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CustomerNavbar } from '../components/CustomerNavbar';
+
+function Product() {
+  return (
+    <main>
+        <div>
+          <CustomerNavbar />
+        </div>
+    </main>
+  );
+}
+
+export default Product;
