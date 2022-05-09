f73ebca1c453a8b19362ad554de8a4bd9077af81
Erick Santos <erickosantos.dev@gmail.com>
2022-05-09 18:08:20 -0300

    feat: 🎸 Adding PRODUCTS_GET api

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -22,30 +22,12 @@ export const REGISTER_POST = (body) => ({
   },
 });
 
-/**
- * import axios from 'axios';
-
-const API_URL = 'http://localhost:3001';
-
-  export const LOGIN_POST = (email, password) => axios
-  .post(`${API_URL}/login`,
-    { email, password },
-    { headers: { 'Content-Type': 'application/json' },
-    });
-
-  export const REGISTER_POST = (user) => axios
-  .post(`${API_URL}/register`, user);
-
-  export const GET_PRODUCTS = async () => {
-    const { token } = JSON.parse(localStorage.getItem('user'));
-    const headers = {
-      authorization: token,
-    };
-    try {
-      const products = await axios.get(`${API_URL}/product`, { headers });
-      return products;
-    } catch (error) {
-      return false;
-    }
-  };
- */
+export const PRODUCTS_GET = (token) => ({
+  url: `${API_URL}/customer/products`,
+  options: {
+    method: 'GET',
+    headers: {
+      Authorization: token,
+    },
+  },
+});
