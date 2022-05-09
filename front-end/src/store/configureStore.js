a84fd111e4a034902885c39924708bdc94323cbb
Erick Santos <erickosantos.dev@gmail.com>
2022-05-09 18:28:56 -0300

    feat: 🎸 Adding token reducer

--- front-end/src/store/configureStore.js
+++ front-end/src/store/configureStore.js
@@ -3,8 +3,9 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 // reducers
 import register from './register';
 import login from './login';
+import token from './token';
 
-const reducer = combineReducers({ register, login });
+const reducer = combineReducers({ register, login, token });
 const store = configureStore({ reducer });
 
 export default store;
