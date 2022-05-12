b97e1dd7869420f5b70f16ebcf070134f8f60db9
Erick Santos <erickosantos.dev@gmail.com>
2022-05-12 20:58:18 -0300

    feat: 🎸 GO to checkout button

--- front-end/src/Components/Products/ListProducts.js
+++ front-end/src/Components/Products/ListProducts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { productsGet } from '../../store/products';
 import ProductCard from './ProductCard';
 
@@ -8,10 +9,15 @@ const ListProducts = () => {
   const { data, error, loading } = useSelector((state) => state.products);
   const { total } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     if (info && !data) dispatch(productsGet(info.token));
   }, [info, dispatch, data]);
+
+  const handleClick = () => {
+    navigate('/customer/checkout');
+  };
 
   return (
     <div className="product-card-container">
@@ -21,9 +27,13 @@ const ListProducts = () => {
       <button
         type="button"
         data-testid="customer_products__button-cart"
+        disabled={ total === 0 }
+        onClick={ handleClick }
       >
-        Total:
-        { ` ${total.toFixed(2).toString().replace('.', ',')}` }
+        Total: R$
+        <span data-testid="customer_products__checkout-bottom-value">
+          {total.toFixed(2).toString().replace('.', ',')}
+        </span>
       </button>
       {error && <p>{error}</p>}
     </div>
