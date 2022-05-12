fad868b19a993a21eff78343f14c3b1f4de69182
Elaine <elainefelipemoreira@gmail.com>
2022-05-12 12:49:03 -0300

    feat: 🎸 add checkout in folder store and modify configureStore

--- front-end/src/store/configureStore.js
+++ front-end/src/store/configureStore.js
@@ -5,8 +5,9 @@ import register from './register';
 import login from './login';
 import token from './token';
 import products from './products';
+import checkout from './checkout';
 
-const reducer = combineReducers({ register, login, token, products });
+const reducer = combineReducers({ register, login, token, products, checkout });
 const store = configureStore({ reducer });
 
 export default store;
