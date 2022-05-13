fe8100da0b7c9eeca47ed22afa7c82a54f5557ae
Erick Santos <erickosantos.dev@gmail.com>
2022-05-13 15:36:40 -0300

    feat: 🎸 Finishing sale button

--- front-end/src/Components/Checkout/CheckoutAddress.js
+++ front-end/src/Components/Checkout/CheckoutAddress.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import useForm from '../../Hooks/useForm';
+import { salesPost } from '../../store/sale';
 import Input from '../Forms/Input';
 
 const CheckoutAddress = () => {
   const [seller, setSeller] = React.useState('Fulana Pereira');
+  const { info: { token, id } } = useSelector((state) => state.user);
+  const { data } = useSelector((state) => state.sale);
+  const { total, cartProducts } = useSelector((state) => state.cart);
   const address = useForm();
   const number = useForm();
   const error = (typeof address.error === 'string')
   || (typeof number.error === 'string');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (data) navigate(`/customer/orders/${data}`);
+  }, [data, navigate]);
 
   const handleClick = () => {
-    console.log(seller, address.value, number.value);
+    const body = {
+      userId: id,
+      seller,
+      total_price: total,
+      delivery_address: address.value,
+      delivery_number: +number.value,
+      products: cartProducts.map(({ name, quant }) => ({ name, quantity: quant })),
+    };
+    dispatch(salesPost(token, body));
   };
 
   return (
