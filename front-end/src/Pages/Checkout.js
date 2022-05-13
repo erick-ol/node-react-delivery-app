59d34ef757cb1a8b2f775d71df4e7c00383407c5
Erick Santos <erickosantos.dev@gmail.com>
2022-05-12 23:02:56 -0300

    feat: 🎸 Checkout Table done

--- front-end/src/Pages/Checkout.js
+++ front-end/src/Pages/Checkout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CustomerNavbar from '../Components/Products/CustomerNavbar';
 import CheckoutTable from '../Components/Checkout/CheckoutTable';
-import CheckoutAddress from '../Components/Checkout/CheckoutAddress';
+// import CheckoutAddress from '../Components/Checkout/CheckoutAddress';
 
 function Checkout() {
   return (
@@ -11,7 +11,7 @@ function Checkout() {
         <h2>Finalizar Pedido</h2>
         <CheckoutTable />
         <h2>Detalhes e Endereço para Entrega</h2>
-        <CheckoutAddress />
+        {/* <CheckoutAddress /> */}
       </div>
     </>
   );
