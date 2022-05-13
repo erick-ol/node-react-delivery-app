a269fc9dcb848210b469eea2fc17af25f871daf2
Erick Santos <erickosantos.dev@gmail.com>
2022-05-13 15:35:07 -0300

    feat: 🎸 Changing fetch salePost

--- front-end/src/store/sale.js
+++ front-end/src/store/sale.js
@@ -1,12 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { SALES_POST } from '../api';
-import createAsyncSlice from './helper/createAsyncSlice';
 
-const slice = createAsyncSlice({
+const slice = createSlice({
   name: 'sale',
-  fetchConfig: (token, body) => SALES_POST(token, body),
+  initialState: {
+    loading: false,
+    data: null,
+    error: null,
+  },
+  reducers: {
+    fetchStarted(state) {
+      state.loading = true;
+    },
+    fetchSuccess(state, action) {
+      state.loading = false;
+      state.data = action.payload;
+      state.error = null;
+    },
+    fetchError(state, action) {
+      state.loading = false;
+      state.data = null;
+      state.error = action.payload;
+    },
+    resetState(state) {
+      state.loading = false;
+      state.data = null;
+      state.error = null;
+    },
+  },
 });
 
-export const salesPost = slice.asyncAction;
-export const { resetState: resetSaleState } = slice.actions;
-
 export default slice.reducer;
+
+const { fetchStarted, fetchSuccess, fetchError } = slice.actions;
+
+export const salesPost = (token, body) => async (dispatch) => {
+  try {
+    dispatch(fetchStarted());
+
+    const { url, options } = SALES_POST(token, body);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+
+    dispatch(fetchSuccess(json));
+  } catch (error) {
+    dispatch(fetchError(error.message));
+  }
+};
