5099c9f4366818089e1ab8c06fa59cb413d058a8
Erick Santos <erickosantos.dev@gmail.com>
2022-05-13 16:14:26 -0300

    feat: 🎸 Creating Seller Orders Page

---
+++ front-end/src/Pages/SellerOrders.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SellerNavbar from '../Components/Seller/SellerNavbar';
+
+const SellerOrders = () => (
+  <div>
+    <SellerNavbar />
+  </div>
+);
+
+export default SellerOrders;
