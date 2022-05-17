24feb500540807e8dd8f3c14576d2906f2fb07d5
Erick Santos <erickosantos.dev@gmail.com>
2022-05-17 15:29:15 -0300

    feat: 🎸 GEtting orders

--- front-end/src/Pages/CustomerOrders.js
+++ front-end/src/Pages/CustomerOrders.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import CustomerNavbar from '../Components/Products/CustomerNavbar';
+import { customerOrdersGet } from '../store/customerOrders';
 
 function CustomerOrders() {
+  const { data } = useSelector((state) => state.customerOrders);
+  const { info: { id, token } } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (!data) dispatch(customerOrdersGet(token, id));
+  }, [data, dispatch, id, token]);
+
   return (
     <>
       <CustomerNavbar />
